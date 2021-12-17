a10b7e5dceb6206f8869c7ae5c50a9bf0b56a1dd
Jessica <jessiidiaz28@gmail.com>
2021-12-17 11:14:49 +0100

    commit inicial

--- cliente/src/Components/Home.js
+++ cliente/src/Components/Home.js
@@ -4,13 +4,26 @@ import Buscador from "./Buscador";
 import Categorias from "./Categorias";
 import Instagram from "./Instagram";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
+
 function Home() {
+  function toTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <body className="fondoVerde">
       <Buscador />
       <Presentacion />
       <Categorias />
       <Instagram />
+      <button className="botonTop" onClick={toTop}>
+        <FontAwesomeIcon className="rosa arrow" icon={faArrowCircleUp} />
+      </button>
     </body>
   );
 }
